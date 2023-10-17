4744098ada57c00a25b9bcad0f5fdf136313a1db
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-10-17 19:54:52 -0300

    GT01: Exercicío do GoTour, não compreendi muito bem.

---
+++ exercise_1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for ; z < x; z++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(10))
+}
