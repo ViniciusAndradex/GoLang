095dbac4d7cb7cf9b0436e789f0f84df5a089261
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-10-17 18:05:59 -0300

    GT01: Finalizando o primeiro módulo do go tour.

--- var.go
+++ var.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
 var c, python int
 
 func main() {
 	var java, cobol = true, "no!"
 	k := 3
-	print(c, cobol, java, python, k)
+	const Lucas = "Eu sou legal"
+
+	fmt.Print(c, cobol, java, python, k)
+	fmt.Print(Lucas)
 }
